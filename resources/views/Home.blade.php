09aa74b4967c7e29c81260b457ec0df8ecfa7f2f
kronina <kroninanes@hotmail.com>
2021-02-18 18:42:56 +0100

    ajouter image

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -5,7 +5,7 @@
     
   @section('content')
 
-  
+  <img class ="image" src ="image/tartelete.jpg" alt ="tartelete">
 
   <p>soyez les bien venu sur mon blog de cuisine  pour plus de detail <a  href="{{route('pContacte')}}" >contactez nous</a>  <p/>
   {{env('APP_NAME')}}
